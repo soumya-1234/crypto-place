3bc69bab83e0895a5665fe50c4e1229453bf8a8e
Soumya Acharya <soumyaacharya@MacBook-Pro-62.local>
2024-08-03 23:50:36 +0530

    second commit

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import  { useContext, useEffect, useState } from 'react'
 import './Home.css'
 import { CoinContext } from '../../context/CoinContext'
 
