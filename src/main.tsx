86a22db7c000158f1db73edd39a0f0bf06f9321e
Soumya Acharya <soumyaacharya@MacBook-Pro-238.lan>
2025-03-24 08:23:46 +0530

    issue 1 fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import CoinContextProvider from './context/CoinContext.tsx'
+import CoinContextProvider from './context/CoinContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
